3f4b4ef3b19805b0667eb71e6dc338efae5e4237
cheu <mingmwoo@gmail.com>
2025-03-20 17:35:33 +0800

    Add files via upload

--- script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll", function () {
-    window.scrollTo({ top: window.innerHeight, behavior: "smooth" });
-
-    setTimeout(() => {
-        window.location.href = "next.html";
-    }, 500); // Slight delay before redirecting
-});
